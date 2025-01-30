5434bd3db624e34c1edaef2acdad36876a3518aa
R. Edward Walsh <r.eddie.walsh@gmail.com>
2025-01-30 08:48:21 +0000

    make parser variable more clear

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("class", help="select the name of the class")
+parser.add_argument("class_name", metavar="CLASS", help="select the name of the class")
 parser.add_argument("-a", "--add-score", help="Add a score to class (default)")
 args = parser.parse_args()
