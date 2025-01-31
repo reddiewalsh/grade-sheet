8839c5cbbaf8f73ee1815ed5a9e140f2d33d7a99
R. Edward Walsh <r.eddie.walsh@gmail.com>
2025-01-31 01:27:26 +0000

    implement get score func

--- project.py
+++ project.py
@@ -16,12 +16,27 @@ def main():
         sys.exit(f"Error: Class {args.class_name} not found in {PATH_TO_GRADES}")
     for student in grade_sheet:
         print(student)
+    assignment = input("Enter Name of Assignment: ").strip().lower()
+    max_score = get_score("Enter Max Score: ")
 
 
-def open_grade_sheet(name_of_class: str, path_to_file: str):
+def open_grade_sheet(name_of_class: str, path_to_file: str) -> list[dict]:
+    '''Reads a CSV file and returns a list of students as dict objects'''
     with open(Path(path_to_file) / f"{name_of_class}.csv") as file:
         reader = csv.DictReader(file)
         return [x for x in reader]
+    
+def get_score(message: str) -> int:
+    '''Prompt the user for a score. Scores of -1 are allowed and are considered excused'''
+    while True:
+        try:
+            score = int(input(message))
+        except ValueError:
+            pass
+        else:
+            if score > -2:
+                break
+    return score
     
 
 if __name__ == "__main__":
